2db7e7e1e43c2108cdff25db617971acb639cd1a
Sami <lolipop66611@gmail.com>
2022-02-25 16:15:14 +0100

    Main class made

---
+++ Module1/src/Code.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Code {
+
+    static Scanner scanner;
+
+    public Code() {
+        scanner = new Scanner(System.in);
+    }
+
+    public static void main(String[] args) {
+        new Code().runCode();
+    }
+
+    private void runCode() {
+        System.out.println("Hello");
+    }
+}
